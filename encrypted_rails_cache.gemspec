5bd410607b5255b3ba03017720bc2f629eef5c1e
Kevin Klein <kevin@tabolo.de>
2019-07-15 20:46:48 +0200

    first commit

---
+++ encrypted_rails_cache.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name = %q{encrypted_rails_cache}
+  s.version = "0.0.1"
+  s.date = %q{2011-09-29}
+  s.summary = %q{my_gem is an awesome gem}
+  s.files = [
+    "Gemfile",
+    "Rakefile",
+    "VERSION",
+    "lib/encrypted_rails_cache.rb"
+  ]
+  s.require_paths = ["lib"]
+  s.authors = ['kevin klein']
+end
---
+++ lib/encrypted_rails_cache.rb
@@ -0,0 +1,49 @@
+require 'openssl'
+require 'json'
+
+module ActiveSupport
+  module Cache
+    class RedisCacheStore
+      def deserialize_entry(serialized_entry)
+        return unless serialized_entry
+
+        data = ::JSON.parse(serialized_entry)
+
+        decode_cipher = OpenSSL::Cipher.new('AES-256-CBC')
+        decode_cipher.decrypt
+        decode_cipher.key = [Rails.application.secrets.cache_key].pack('H*')
+        decode_cipher.iv = Base64.decode(data['iv'])
+
+        plain = decode_cipher.update(Base64.decode(data['data']))
+        plain = plain + decode_cipher.final
+
+        entry = Marshal.load(serialized_entry) rescue serialized_entry
+        entry.is_a?(Entry) ? entry : Entry.new(entry)
+      end
+
+      def serialize_entry(entry, raw: false)
+        blob = if raw
+          entry.value.to_s
+        else
+          Marshal.dump(entry)
+        end
+
+        aes = OpenSSL::Cipher.new('AES-256-CBC')
+        iv = aes.random_iv
+        aes.encrypt
+        aes.iv = iv
+        aes.key = [Rails.application.secrets.cache_key].pack('H*')
+
+        cipher = aes.update(blob)
+        cipher = cipher + aes.final
+
+        data = {
+          data: Base64.encode64(cipher),
+          iv: Base64.encode64(iv)
+        }
+
+        data.to_json
+      end
+    end
+  end
+end
